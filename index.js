c1b3b86de2cbbddffe05d8eb6711125a2b90a01e
vincentriemer <vincentriemer@gmail.com>
2017-04-05 11:44:03 -0400

    removed fixed positioning

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 "use strict";
 var React = require("react");
 var Iframe = React.createClass({
-    displayName: "React-Iframe",
+  displayName: "React-Iframe",
 
-    propTypes: {
-        url: React.PropTypes.string.isRequired,
-        width: React.PropTypes.string,
-        height: React.PropTypes.string
-    },
+  propTypes: {
+    url: React.PropTypes.string.isRequired,
+    width: React.PropTypes.string,
+    height: React.PropTypes.string },
 
-    getDefaultProps: function getDefaultProps() {
-        return {
-            height: "100%",
-            width: "100%",
-            position: "fixed"
-        };
-    },
+  getDefaultProps: function getDefaultProps() {
+    return {
+      height: "100%",
+      width: "100%" };
+  },
 
-    shouldComponentUpdate: function shouldComponentUpdate(nextProps) {
-        return this.props.url !== nextProps.url;
-    },
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps) {
+    return this.props.url !== nextProps.url;
+  },
 
-    render: function render() {
-        return React.createElement("iframe", { ref: "iframe",
-            frameBorder: "0",
-            src: this.props.url,
-            style: { position: this.props.position, height: this.props.height, width: this.props.width },
-            height: this.props.height, width: this.props.width });
-    }
-});
+  render: function render() {
+    return React.createElement("iframe", {
+      ref: "iframe",
+      frameBorder: "0",
+      src: this.props.url,
+      style: {
+        height: this.props.height,
+        width: this.props.width },
+      height: this.props.height,
+      width: this.props.width
+    });
+  } });
 
 module.exports = Iframe;
--- index.jsx
+++ index.jsx
@@ -1,37 +1,40 @@
 "use strict";
 var React = require("react");
 var Iframe = React.createClass({
-    displayName: "React-Iframe",
+  displayName: "React-Iframe",
 
-    propTypes: {
-        url: React.PropTypes.string.isRequired,
-        width: React.PropTypes.string,
-        height: React.PropTypes.string
-    },
+  propTypes: {
+    url: React.PropTypes.string.isRequired,
+    width: React.PropTypes.string,
+    height: React.PropTypes.string,
+  },
 
-    getDefaultProps:function(){
-        return {
-        height:'100%',
-        width:'100%',
-        position:'fixed'
-        }
-    },
+  getDefaultProps: function() {
+    return {
+      height: "100%",
+      width: "100%",
+    };
+  },
 
-    shouldComponentUpdate: function(nextProps) {
-        return this.props.url !== nextProps.url;
-    },
+  shouldComponentUpdate: function(nextProps) {
+    return this.props.url !== nextProps.url;
+  },
 
-    render: function() {
-        return (
-            <iframe ref="iframe"
-            frameBorder='0' 
-            src={this.props.url}
-            style={{position:this.props.position,height:this.props.height,width:this.props.width}}
-            height={this.props.height} width={this.props.width}></iframe>
-        )
-
-
-    }
+  render: function() {
+    return (
+      <iframe
+        ref="iframe"
+        frameBorder="0"
+        src={this.props.url}
+        style={{
+          height: this.props.height,
+          width: this.props.width,
+        }}
+        height={this.props.height}
+        width={this.props.width}
+      />
+    );
+  },
 });
 
 module.exports = Iframe;
